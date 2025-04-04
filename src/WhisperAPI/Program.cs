7d4afe6e87cfb09540fe9805a1b40794608b4a9d
Neville Nazerane <live.neville@live.com>
2025-04-04 03:23:20 -0700

    optimized

--- src/WhisperAPI/Program.cs
+++ src/WhisperAPI/Program.cs
@@ -9,6 +9,26 @@ using Whisper.net.LibraryLoader;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+// START UP
+var modelName = "ggml-large-v1.bin";
+
+Directory.CreateDirectory("audio");
+
+if (!File.Exists(modelName))
+{
+    Console.WriteLine("Downloading...");
+    await using var modelStream = await WhisperGgmlDownloader.Default.GetGgmlModelAsync(GgmlType.Base);
+    await using var fileWriter = File.OpenWrite(modelName);
+    await modelStream.CopyToAsync(fileWriter);
+    Console.WriteLine("Downloaded!");
+}
+
+Console.WriteLine("Getting model...");
+
+
+
+
 app.MapGet("/", () => "Hello API!");
 
 app.MapPost("/listen", StreamMeAsync);
@@ -16,9 +36,9 @@ app.MapPost("/listen", StreamMeAsync);
 await app.RunAsync();
 
 
+
 async Task<string> StreamMeAsync(HttpRequest req)
 {
-    Directory.CreateDirectory("audio");
     var wavFile = $"audio/{Guid.NewGuid():N}.wav";
 
     await using (var file = File.Create(wavFile))
@@ -30,23 +50,10 @@ async Task<string> StreamMeAsync(HttpRequest req)
 
 async Task<string> ReadFileAsync(string wavFileName)
 {
-    var modelName = "ggml-medium.en.bin";
-    if (!File.Exists(modelName))
-    {
-        Console.WriteLine("Downloading...");
-        await using var modelStream = await WhisperGgmlDownloader.Default.GetGgmlModelAsync(GgmlType.Base);
-        await using var fileWriter = File.OpenWrite(modelName);
-        await modelStream.CopyToAsync(fileWriter);
-        Console.WriteLine("Downloaded!");
-    }
-
-    Console.WriteLine("Getting model...");
-
-
-    using var whisperFactory = WhisperFactory.FromPath(modelName);
-
 
     Console.WriteLine("Getting processor...");
+
+    using var whisperFactory = WhisperFactory.FromPath(modelName);
 
     using var processor = whisperFactory.CreateBuilder()
                                         .WithLanguage("auto")
