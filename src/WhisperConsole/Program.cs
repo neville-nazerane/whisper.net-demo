2d9aab86c9caf96e73875c83f69a06d2ad826c84
Neville <live.neville@live.com>
2025-04-07 18:22:59 -0700

    testing files

--- src/WhisperConsole/Program.cs
+++ src/WhisperConsole/Program.cs
@@ -1,6 +1,5 @@
 ﻿using NAudio.Wave;
-using Whisper.net;
-using Whisper.net.Ggml;
+using System.Diagnostics;
 
 
 using var httpClient = new HttpClient
@@ -9,10 +8,22 @@ using var httpClient = new HttpClient
     BaseAddress = new("http://192.168.1.183:5000")
 };
 
-while (true)
+
+string? file;
+
+//file = @"C:\Users\liven\output-2a412e90a7594cd4a8d8f319739d0b47.wav";
+
+file = args.FirstOrDefault();
+
+
+if (file is not null)
 {
-    await Task.WhenAny(Task.Delay(TimeSpan.FromSeconds(1)), RunMeAsync());
+    await SendItToTheInternetAsync(file);
+    return;
 }
+
+while (true)
+    await Task.WhenAny(Task.Delay(TimeSpan.FromSeconds(1)), HandledRunningAsync());
 
 
 
@@ -31,43 +42,124 @@ async Task SendItToTheInternetAsync(string wavFileName)
     Console.WriteLine(str);
 }
 
-async Task RunMeAsync()
+async Task HandledRunningAsync()
 {
-    string wavFileName = $"output-{Guid.NewGuid():N}.wav";
+    try
+    {
+        string wavFileName = $"output-{Guid.NewGuid():N}.wav";
+
+        Console.WriteLine("Saving " + wavFileName);
+        if (OperatingSystem.IsWindows())
+            await RunMeAsync(wavFileName);
+        else
+            await RunMeOnLinuxAsync(wavFileName);
+
+        if (File.Exists(wavFileName))
+        {
+            await SendItToTheInternetAsync(wavFileName);
+            File.Delete(wavFileName);
+        }
+        else
+        {
+            Console.WriteLine("No file saved " + wavFileName);
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+}
+
+async Task RunMeOnLinuxAsync(string wavFileName)
+{
+    var completion = new TaskCompletionSource();
+
+    var psi = new ProcessStartInfo
+    {
+        FileName = "arecord",
+        ArgumentList = { "-Dshared_mic", "-f", "S32_LE", "-r", "16000", "-c", "4", wavFileName },
+        RedirectStandardOutput = true,
+        //RedirectStandardError = true
+    };
+
+    using var process = new Process
+    {
+        StartInfo = psi,
+        EnableRaisingEvents = true,
+    };
+
+    process.OutputDataReceived += (_, s) => Console.WriteLine(s.Data);
+
+    process.ErrorDataReceived += (_, s) => completion.SetException(new Exception(s.Data));
+
+    process.Exited += (_, _) => completion.TrySetResult();
+
+    process.Start();
+    await Task.Delay(TimeSpan.FromSeconds(15));
+    process.Kill();
+    //process.BeginOutputReadLine();
+    //process.BeginErrorReadLine();
+
+    await completion.Task;
+    await Task.Delay(TimeSpan.FromMilliseconds(500));
+}
+
+async Task RunMeAsync(string wavFileName)
+{
     TaskCompletionSource completionSource = new();
+
     using var waveIn = new WaveInEvent();
-    waveIn.WaveFormat = new WaveFormat(16000, 1);
-    var writer = new WaveFileWriter(wavFileName, waveIn.WaveFormat);
-
-    var disposed = false;
-    var writeLock = new object();
-
-    waveIn.DataAvailable += (s, a) =>
+    WaveFileWriter? writer = null;
+    try
     {
-        lock (writeLock)
-        {
-            if (!disposed)
-                writer.Write(a.Buffer, 0, a.BytesRecorded);
-        }
-    };
 
-    waveIn.RecordingStopped += async (s, a) =>
+        waveIn.WaveFormat = new WaveFormat(16000, 1);
+        writer = new(wavFileName, waveIn.WaveFormat);
+
+    }
+    catch (Exception ex)
     {
-        lock (writeLock)
+        completionSource.SetException(ex);
+    }
+
+    if (writer is not null)
+    {
+        var disposed = false;
+        var writeLock = new object();
+
+        waveIn.DataAvailable += (s, a) =>
         {
-            disposed = true;
-            writer.Dispose();
+            lock (writeLock)
+            {
+                if (!disposed)
+                    writer.Write(a.Buffer, 0, a.BytesRecorded);
+            }
+        };
+
+        waveIn.RecordingStopped += (s, a) =>
+        {
+            lock (writeLock)
+            {
+                disposed = true;
+                writer.Dispose();
+            }
+
+
+            completionSource.SetResult();
+        };
+
+        try
+        {
+            waveIn.StartRecording();
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            waveIn.StopRecording();
         }
-
-        await SendItToTheInternetAsync(wavFileName);
-        completionSource.SetResult();
-    };
-
-    waveIn.StartRecording();
-    await Task.Delay(TimeSpan.FromSeconds(10));
-    //Console.WriteLine("Recording... press enter to stop");
-    //Console.ReadLine();
-    waveIn.StopRecording();
+        catch (Exception ex)
+        {
+            completionSource.SetException(ex);
+        }
+    }
 
     await completionSource.Task;
 }
